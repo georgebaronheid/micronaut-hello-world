453c8a27f43a90c6a969b9221154bc6e4d10b6ec
George Baronheid <baronheid.george@gmail.com>
2021-02-23 18:57:54 -0300

    Hello world finished

--- src/test/kotlin/com/baronheid/controllers/HelloControllerTest.kt
+++ src/test/kotlin/com/baronheid/controllers/HelloControllerTest.kt
@@ -1,12 +1,26 @@
 package com.baronheid.controllers
 
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import javax.inject.Inject
 
-import org.junit.jupiter.api.Assertions.*
-
+@MicronautTest
 internal class HelloControllerTest {
 
+    @Inject
+    @field:Client("/")
+//    HTTPClient bean pointed towards the embedded server URL. Runs on a random port.
+    private lateinit var client: RxHttpClient
+
     @Test
-    fun index() {
+    fun `Should test hello world controller`() {
+        val request: HttpRequest<Any> = HttpRequest.GET("/hello")
+        val body = client.toBlocking().retrieve(request)
+        Assertions.assertNotNull(body)
+        Assertions.assertEquals("Hello world", body)
     }
 }
